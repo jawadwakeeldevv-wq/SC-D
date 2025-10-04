70119aff6bc4a2eac4fa0777802919495873e0ba
jawadwakeeldevv-wq <jawadwakeel.devv@gmail.com>
2025-10-04 23:09:34 +0500

    add function

---
+++ src/add.cpp
@@ -0,0 +1,7 @@
+include 'calculator.h';
+void add(int a, int b) {
+    // Function to add two integers
+    int sum = a + b;
+    // Print the result
+    std::cout << "The sum is: " << sum << std::endl;
+}
